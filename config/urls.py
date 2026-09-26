6738f4dcca80cda9f3036a2d9cb17bd1e5ee9837
Dekantiok <kantiokfelix@gmail.com>
2026-09-26 05:42:24 -0700

    Restore escrow hardening, keep Neon deploy

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_view, name='home'),
     path('my-escrow/', views.my_escrow, name='my_escrow'),
+    path('my-escrow/verify/', views.my_escrow_verify_otp, name='my_escrow_verify_otp'),
+    path('my-escrow/list/', views.my_escrow_list, name='my_escrow_list'),
     path('join/', views.join_escrow_page, name='join_escrow_page'),
     path('create/', views.create_contract, name='create_contract'),
     path('verify/<str:code>/', views.verify_otp_view, name='verify_otp'),
@@ -18,5 +20,7 @@ urlpatterns = [
     path('escrow/<str:code>/receipt/', views.buyer_confirm_receipt, name='buyer_confirm_receipt'),
     path('escrow/<str:code>/release/', views.buyer_release_funds, name='buyer_release_funds'),
     path('escrow/<str:code>/dispute/', views.buyer_raise_dispute, name='buyer_raise_dispute'),
+    path('escrow/<str:code>/seller-dispute/', views.seller_raise_dispute, name='seller_raise_dispute'),
+    path('escrow/<str:code>/cancel/', views.cancel_escrow, name='cancel_escrow'),
     path('escrow/<str:code>/vault/', views.seller_add_vault, name='seller_add_vault'),
 ]
